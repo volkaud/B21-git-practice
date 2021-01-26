fa2b4c337f50feca530331219cdfbd7abdc29424
volkaud <daria.volkau@gmail.com>
2021-01-26 14:24:11 -0500

    added some comments

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -1,6 +1,8 @@
 public class FeatureACode {
     public static void main(String[] args) {
         System.out.println("Awesome work for A started");
+        System.out.println("B21 is learning more got with review");
+        System.out.println("I wonder if i will master this");
     }
     }
 
